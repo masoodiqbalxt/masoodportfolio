615ea30d75a918b5bb2733d43fce5dbaa3241d70
masoodiqbalxt <141815534+masoodiqbalxt@users.noreply.github.com>
2026-03-15 15:32:50 +0500

    next.config.ts
    Fix local dev and update Node version".

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isProd ? "/masoodportfolio" : "",
+  assetPrefix: isProd ? "/masoodportfolio/" : "",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
